15e6e878532d899ce8f46c3eba819a0abdb79b6f
bishoykamal7 <44450788+bishoykamal7@users.noreply.github.com>
2018-11-20 23:54:18 +0200

    View and layout

--- snow board/snow board/Controllers/HomeController.cs
+++ snow board/snow board/Controllers/HomeController.cs
@@ -15,12 +15,7 @@ namespace snow_board.Controllers
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
+        
 
         public IActionResult Contact()
         {
@@ -29,15 +24,19 @@ namespace snow_board.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult login()
         {
+            
+
+            return View();
+        }
+        public IActionResult checkout()
+        {
+
+
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+
     }
 }
---
+++ snow board/snow board/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace snowboard.Models
+{
+    public class Customer
+{
+        int name;
+        int id;
+
+
+
+
+}
+
+}
